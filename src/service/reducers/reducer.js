01626cc2c1a240115984dbcf22515a69ecbf4909
shiwani295 <shiwanitiwarishiwani@gmail.com>
2023-04-21 17:51:54 +0530

    Remove to cart

--- src/service/reducers/reducer.js
+++ src/service/reducers/reducer.js
@@ -1,15 +1,19 @@
 //create reducer which perform action || action send data to reducer
+import { ADD_TO_CART, REMOVE_TO_CART } from "../Constants.js";
 const initialState = {
   cartdata: [],
 };
 export default function cartreducerItem(state = [], action) {
   //for the multile value we assign array in initial value
   switch (action.type) {
-    case "ADD_TO_CART":
+    case ADD_TO_CART:
       console.log(action);
       //return also array
       return [...state, { cartdata: action.data }];
-      break;
+
+    case REMOVE_TO_CART:
+      state.pop();
+      return [...state];
     default:
       return state;
   }
