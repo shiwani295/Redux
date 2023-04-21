5530f2b9e7a3ddfdbc217bbb64374580de5fd704
shiwani295 <shiwanitiwarishiwani@gmail.com>
2023-04-21 14:51:50 +0530

    Container - create mapDispatchToProps  and mapStateToProps

--- src/Container/HomeContainer.js
+++ src/Container/HomeContainer.js
@@ -1,3 +1,14 @@
+import { connect } from "react-redux";
 import Home from "../components/Home";
+import { AddToCart } from "../service/actions/action";
 
-export default Home;
+//this is call when you already pass the data in store and now you want to use that data
+const mapStateToProps = (state) => ({});
+
+//thhis call when you want to pass data into store
+const mapDispatchToProps = (dispatch) => ({
+  AddTOCartHandler: (data) => dispatch(AddToCart(data)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+//export default Home;
